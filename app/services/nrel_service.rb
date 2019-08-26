4575c410a98445ff48242dfdb76b8a69752746d3
Jori Peterson <joripeterson@gmail.com>
2019-08-26 11:21:03 -0600

    Final commit

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,16 +5,17 @@ class NrelService
 	end
 
 	def stations_by_zip
-		conn = Faraday.new('https://developer.nrel.gov') do |f|
-			f.headers['X-API-KEY'] = ENV["NREL_KEY"]
-			f.adapter Faraday.default_adapter
-		end
-
-		response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zipcode}&fuel_type=ELEC,LPG&limit=10")
-		JSON.parse(response.body, symbolize_names: true)
+		get_json("/api/alt-fuel-stations/v1/nearest.json?location=#{@zipcode}&fuel_type=ELEC,LPG&limit=10")
 	end
 
 	private
+
+	def conn
+		Faraday.new('https://developer.nrel.gov') do |f|
+			f.headers['X-API-KEY'] = ENV["NREL_KEY"]
+			f.adapter Faraday.default_adapter
+		end
+	end
 
 	def get_json(url)
 		response = conn.get(url)
