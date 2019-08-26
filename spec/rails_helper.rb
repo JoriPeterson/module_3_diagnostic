d89d04f69fdec0c3b4a0a5ee60900eb2fdd621bf
Jori Peterson <joripeterson@gmail.com>
2019-08-25 20:32:57 -0600

    Complete setup for M3 assessment.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,12 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -33,6 +38,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+	config.include FactoryBot::Syntax::Methods
+
   config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
